c35191675c50b80760d045123fe1f8abe62ef883
nikolay.belibov <nikolay.belibov@gmail.com>
2016-12-16 16:51:09 +0300

    График на D3

---
+++ dest/js/plot_1.js
@@ -0,0 +1,189 @@
+function CSVToArray( strData, strDelimiter ){
+    // Check to see if the delimiter is defined. If not,
+    // then default to comma.
+    strDelimiter = (strDelimiter || ",");
+
+    // Create a regular expression to parse the CSV values.
+    var objPattern = new RegExp(
+        (
+            // Delimiters.
+            "(\\" + strDelimiter + "|\\r?\\n|\\r|^)" +
+
+            // Quoted fields.
+            "(?:\"([^\"]*(?:\"\"[^\"]*)*)\"|" +
+
+            // Standard fields.
+            "([^\"\\" + strDelimiter + "\\r\\n]*))"
+        ),
+        "gi"
+    );
+
+
+    // Create an array to hold our data. Give the array
+    // a default empty first row.
+    var arrData = [[]];
+
+    // Create an array to hold our individual pattern
+    // matching groups.
+    var arrMatches = null;
+
+
+    // Keep looping over the regular expression matches
+    // until we can no longer find a match.
+    while (arrMatches = objPattern.exec( strData )){
+
+        // Get the delimiter that was found.
+        var strMatchedDelimiter = arrMatches[ 1 ];
+
+        // Check to see if the given delimiter has a length
+        // (is not the start of string) and if it matches
+        // field delimiter. If id does not, then we know
+        // that this delimiter is a row delimiter.
+        if (
+            strMatchedDelimiter.length &&
+            strMatchedDelimiter !== strDelimiter
+        ){
+
+            // Since we have reached a new row of data,
+            // add an empty row to our data array.
+            arrData.push( [] );
+
+        }
+
+        var strMatchedValue;
+
+        // Now that we have our delimiter out of the way,
+        // let's check to see which kind of value we
+        // captured (quoted or unquoted).
+        if (arrMatches[ 2 ]){
+
+            // We found a quoted value. When we capture
+            // this value, unescape any double quotes.
+            strMatchedValue = arrMatches[ 2 ].replace(
+                new RegExp( "\"\"", "g" ),
+                "\""
+            );
+
+        } else {
+
+            // We found a non-quoted value.
+            strMatchedValue = arrMatches[ 3 ];
+
+        }
+
+
+        // Now that we have our value string, let's add
+        // it to the data array.
+        arrData[ arrData.length - 1 ].push( strMatchedValue );
+    }
+
+    // Return the parsed data.
+    console.log(arrData)
+    return( arrData );
+}
+// Система отступов для d3
+var margin = {top: 10, right: 50, bottom: 30, left: 40};
+var width = 500 - margin.left - margin.right;
+var height = 400 - margin.top - margin.bottom;
+
+var svg = d3.select("#chart")
+    .attr("width", width + margin.left + margin.right)
+    .attr("height", height + margin.top + margin.bottom)
+    .append("g")
+    .attr("class", "chart__cnt")
+    .attr("transform", "translate(" + margin.left + "," + margin.top + ")")
+
+
+// Для работы с .csv
+/*
+    d3.csv(path, function(){}, function(){})
+*/
+d3.csv(
+    'http://d3-js.ru/data/gapminder.csv',
+    //обрабатывает каждый полученный обьект и возвращает результат обработки
+    function (country){
+        /*
+            переведем числовые данные в числа а не строки
+        */
+        country.gdp = Number(country.gdp);
+        country.life = Number(country.life);
+        country.population = Number(country.population);
+        return country;
+    },
+    // сюда передается Array щбработанных объектов,
+    // тут визуализируем и обрабатываем данные
+    function (countries){
+        console.log(countries);
+
+        // Итак, для связи данных и систем координат нам необходимы Шкалы
+        // Используем Линейную шкалу
+
+        // для оси Х
+        var x = d3.scale.linear()
+            .domain([0 , d3.max(countries, function (d) {
+                return d.gdp;
+            })]) // для отображения ВВП старны gdp
+            .range([0, width]);
+        // для оси Y
+        var y = d3.scale.linear()
+            .domain(d3.extent(countries, function (d) { return d.life })) //мин и макс продолжительности жизни
+            .range([(height - 10) , 0]);
+        // radius
+        var r = d3.scale.linear()
+            .domain([0, Math.sqrt(d3.max(countries, function (d) { return d.population }))]) //мин и макс продолжительности жизни
+            .range([0, 20]);
+
+
+        //После задания шкал отрисуем круги
+        var g = svg.selectAll('g.point')
+            .data(countries)
+            .enter()
+            .append('g')
+            .attr('class', 'point');
+
+        g.append('circle')
+            .attr('class', 'point__circle')
+            .attr('fill', function (d){ return d.color; })
+            .attr('cx', '0').transition().duration(500)
+
+            .attr('cx', function (d){ return x(d.gdp); })
+            .attr('cy', function (d){ return y(d.life); })
+            .attr('r', function (d){ return r(Math.sqrt(d.population)); })
+
+            .attr('data-country', function (d) { return d.country });
+
+        g.append('text')
+            .attr('class', 'point__text')
+            .text(function (d) { return d.country })
+            .attr('x', function (d){ return x(d.gdp) + r(Math.sqrt(d.population)); })
+            .attr('y', function (d){ return y(d.life); })
+
+            .attr('dx', 4)
+            .attr('dy', 4);
+
+        //Для отрисовки осей координат:
+        // d3.svg.axis()
+        //     .scale()
+        //     .orient();
+
+        //Оси должны быть каждая в своей группе, поэтому создадим для осей группу
+        var axis = svg.append('g')
+            .attr('class', 'axis');
+        
+        axis.append('g')
+            .attr('class', 'axis__x')
+            .attr('transform', 'translate(0,' + ( height + 10) +')')
+            .call(
+                d3.svg.axis()
+                    .scale(x)
+                    .orient('bottom')
+            );
+        axis.append('g')
+            .attr('class', 'axis__y')
+            .call(
+                d3.svg.axis()
+                    .scale(y)
+                    .orient('left')
+            );
+    }
+);
